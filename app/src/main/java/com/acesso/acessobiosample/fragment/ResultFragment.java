7e139c5d3510b5282298784b5b2b471696362c85
Lucas Ibiapina <lucas.silva@acessodigital.com.br>
2020-02-13 18:36:51 -0300

    Events on Fragment Success (Feedback, Exit) and event LivenessError  (TryAgain)

--- app/src/main/java/com/acesso/acessobiosample/fragment/ResultFragment.java
+++ app/src/main/java/com/acesso/acessobiosample/fragment/ResultFragment.java
@@ -2,6 +2,7 @@ package com.acesso.acessobiosample.fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,9 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 
 import com.acesso.acessobiosample.R;
+import com.acesso.acessobiosample.activity.AuthenticationActivity;
 import com.acesso.acessobiosample.activity.SelfieActivity;
+import com.acesso.acessobiosample.activity.SimpleViewActivity;
 import com.orhanobut.hawk.Hawk;
 
 import java.util.HashMap;
@@ -45,8 +48,43 @@ public class ResultFragment extends CustomFragment {
             ImageView ivAfar = ((ImageView) v.findViewById(R.id.ivAfar));
             ivAfar.setImageBitmap(bitmapAfar);
 
+            Button btExit = (Button)v.findViewById(R.id.btExit);
+            Button btFeedback = (Button)v.findViewById(R.id.btFeedback);
+
+            btExit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity() , SimpleViewActivity.class);
+                    intent.putExtra(CustomFragment.FRAGMENT, WelcomeFragment.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            });
+
+            btFeedback.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO);//common intent
+                    intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback Liveness");
+                    intent.putExtra(Intent.EXTRA_TEXT, "" );
+                    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"suportebio@acessodigital.com.br"});
+                    startActivity(Intent.createChooser(intent, ""));
+
+                }
+            });
+
         }else{
             v = inflater.inflate(R.layout.fragment_liveness_error, null);
+
+            Button btTryAgain = v.findViewById(R.id.btTryAgain);
+            btTryAgain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity(), SelfieActivity.class);
+                    startActivity(intent);
+                }
+            });
         }
 
 
